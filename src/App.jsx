6716740c4bba0609efe524422b86cea33b420303
vini <viniciuscmbrr@gmail.com>
2024-03-20 17:23:54 -0300

    Teste branch

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>rato + Vini</h1>
+      <h1>rato </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
