6c67ef6caf354345b453c06644e4f06ce76962f9
vini <viniciuscmbrr@gmail.com>
2024-03-31 20:14:08 -0300

    stars-section  was finished

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import { useState,useRef,Suspense } from "react";
+import { Canvas,useFrame} from "@react-three/fiber";
+import { Points, PointMaterial,Preload } from "@react-three/drei";
 
-const Stars = () => {
+import * as random from "maath/random/dist/maath-random.esm"
+
+const Stars = (props) => {
+  const ref = useRef();
+  const sphere =  random.inSphere(new Float32Array(500), {radius:1.2});
+
+  useFrame((state,delta) => {
+    ref.current.rotation.x -= delta/10;
+    ref.current.rotation.x -= delta/15;
+  })
+
   return (
-    <div>Stars</div>
+    <group rotation={[0,0,Math.PI/4]}>
+      <Points ref = {ref} positions = {sphere} stride = {3} frustumCulled {...props}>
+          <PointMaterial
+            transparent
+            size = {0.002}
+            sizeAttenuation =  {true}
+            depthWrite = {false}
+          />
+      </Points>
+    </group>
   )
 }
 
-export default Stars
+const StarCanvas = () => {
+  return(
+    <div className="w-full h-auto absolute inset-0
+    z[-1]">
+      <Canvas camera={{position:[0,0,1]}}>
+
+        <Suspense fallback = {null}>
+          <Stars/>
+        </Suspense>
+
+        <Preload all/>
+      </Canvas>
+    </div>
+  )
+
+}
+
+export default StarCanvas
